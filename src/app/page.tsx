a30f4f199a5f2e5b6ee057e8217cbdd219d1a3f8
kadelcode <kadelcode@gmail.com>
2025-05-17 21:17:33 +0100

    Add live link and remove useEffect() to prevent unwanted page behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 "use client"
 
-import { useEffect } from "react"
+// import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function HomePage() {
   const router = useRouter()
 
-  useEffect(() => {
+  /*useEffect(() => {
     const token = localStorage.getItem("token")
     if (token) {
       router.push("/dashboard")
     }
-  }, [router])
+  }, [router])*/
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-black p-6">
@@ -21,7 +22,7 @@ export default function HomePage() {
         This is a simple frontend built with Next.js 13+, TailwindCSS, and ShadCN to test an authentication backend system
   
       </p>
-      <div className="flex gap-4">
+      <div className="flex gap-4 mb-20">
         <Button
           className="bg-zinc-900 hover:bg-zinc-800 cursor-pointer text-white" 
           onClick={() => router.push('/login')}
@@ -50,6 +51,15 @@ export default function HomePage() {
           Admin
         </Button>
       </div>
+
+      <Link href="#">
+        <Button
+          className="bg-zinc-950 hover:bg-zinc-900 px-9 py-3 border border-gray-700 cursor-pointer text-white"
+          title="The URL where the backend service is deployed."
+        >
+          Live URL
+        </Button>
+      </Link>
     </main>
   )
 }

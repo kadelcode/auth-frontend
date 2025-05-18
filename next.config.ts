c983e825fc745cd6e2975b83deea9e6a86666b47
kadelcode <kadelcode@gmail.com>
2025-05-18 17:55:58 +0100

    Setup proxy to fix iOS cookie problems

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: 'api/:path*', // All request to /api/ will be proxied
+        destination: 'https://auth-service-production-d511.up.railway.app/:path*', // Backend URL
+      }
+    ]
+  }
 };
 
 export default nextConfig;

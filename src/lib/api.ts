6eec1214c650de0d7bde0550e2679680fad73eeb
kadelcode <kadelcode@gmail.com>
2025-08-02 19:25:22 +0100

    Update api endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,3 @@
 export const getBaseUrl = () => {
-    return process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+    return process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1";
 };
